6232265fbefabf9327bc2dd4d68fc2e41ce484a0
elena <elenamkoian@mail.com>
2021-12-08 19:05:37 +0400

    [add] TaskListItem

---
+++ src/components/task-list-item/task-list-item.js
@@ -0,0 +1,9 @@
+import "./tasl-list-item.scss"
+
+export const TaskListItem = () => {
+  return (
+    <div className="TaskListItem">
+      Task List Item
+    </div>
+  )
+}
--- src/components/task-list/task-list.js
+++ src/components/task-list/task-list.js
@@ -1,9 +1,10 @@
 import './task-list.scss'
+import { TaskListItem } from '../task-list-item/task-list-item';
 
 export const TaskList = () => {
   return (
     <div className="TaskList">
-      "TaskList"
+       <TaskListItem />
     </div>
   )
 }
