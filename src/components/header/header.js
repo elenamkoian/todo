16ebb45ab981ab8cac864d0935e7057e5b69ff4f
elena <elenamkoian@mail.com>
2021-12-08 18:27:04 +0400

    [add] app header

---
+++ src/components/header/header.js
@@ -0,0 +1,9 @@
+import './header.scss'
+
+export const Header = () => {
+  return (
+    <div className="Header">
+      <span>Doit</span>
+    </div>
+  )
+}
---
+++ src/components/home-page/home-page.js
@@ -0,0 +1,10 @@
+import { Header } from '../header/header';
+import './home-page.scss'
+
+export const HomePage = () => {
+  return (
+    <div className="HomePage">
+      <Header />
+    </div>
+  )
+}
